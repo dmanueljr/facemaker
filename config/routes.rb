10b7bf28a807ff948d65756ca7ab9e1c3acc8a51
Daniel Manuel <dmanueljr@gmail.com>
2015-07-01 12:33:23 -0700

    adds results page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#index'
+  get '/results' => 'pages#results'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
